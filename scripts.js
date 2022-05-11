61afc7a324e782a352ee9f086651e044eefcd020
Guilhermeh14 <guilherme_h14@hotmail.com>
2022-05-11 14:43:17 -0300

    Tabuada usando FOR

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var Tabuada = parseInt(     prompt("Digite o valor da tabuada desejado"));
+var ValorMaximo = parseInt(     prompt("Digite o Valor maximo para a tabuada"))
+
+for (var i = 0; i <= ValorMaximo; i++) { 
+	console.log(Tabuada + " x " + i + " = " + Tabuada *i)
+
+}
